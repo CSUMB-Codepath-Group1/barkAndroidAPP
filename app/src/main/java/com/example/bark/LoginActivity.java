964af134bdbe382a566f2b2e1b682aba2bd65e95
*randrade8311* <randrade709@gmail.com>
2020-04-20 18:03:09 -0700

    Added login activity

---
+++ app/src/main/java/com/example/bark/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.bark;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity {
+
+    public static final String TAG = "LoginActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+
+
+
+}
