c9a61ba2ac9fa10e779b2d44f93061b340c116d1
larafonse <nlarafonseca@csumb.edu>
2020-04-22 11:21:01 -0700

    successfully added email authentication

--- app/src/main/java/com/example/bark/LoginActivity.java
+++ app/src/main/java/com/example/bark/LoginActivity.java
@@ -1,14 +1,61 @@
 package com.example.bark;
 
-import android.widget.Button;
-import android.widget.EditText;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class LoginActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
 
     public static final String TAG = "LoginActivity";
-    private EditText etUsername;
-    private EditText etPassword;
-    private Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == 1234){
+
+            if(resultCode == RESULT_OK){
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(getApplicationContext(),"Successfully Signed In",Toast.LENGTH_SHORT);
+
+            }
+        }else {
+            Toast.makeText(getApplicationContext(),"Unable to Sign in",Toast.LENGTH_SHORT);
+        }
+    }
+     public void signIn(View view){
+         FirebaseAuth auth = FirebaseAuth.getInstance();
+         if (auth.getCurrentUser() != null){
+             // Already signed In
+             Toast.makeText(getApplicationContext(),"User is Already signed in", Toast.LENGTH_SHORT);
+
+         }else{
+             // Not Signed In
+             List<AuthUI.IdpConfig> providers = Arrays.asList(
+                     new  AuthUI.IdpConfig.EmailBuilder().build());
+
+             startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(),1234);
+         }
+
+     }
 
 
 
