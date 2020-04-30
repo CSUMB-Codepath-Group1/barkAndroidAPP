034ad5880c53bc49fc21b153b70c779566e62cd0
larafonse <nlarafonseca@csumb.edu>
2020-04-29 19:29:55 -0700

    added bottom nav

--- app/src/main/java/com/example/bark/MainActivity.java
+++ app/src/main/java/com/example/bark/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.bark;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
     }
+
 }
