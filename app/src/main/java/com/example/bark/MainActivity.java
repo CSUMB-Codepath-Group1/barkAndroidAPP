31a70235299d521fe09a718f3898a744ab9e1ab6
larafonse <nlarafonseca@csumb.edu>
2020-05-03 18:57:02 -0700

    fragments added

--- app/src/main/java/com/example/bark/MainActivity.java
+++ app/src/main/java/com/example/bark/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.bark;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.bark.fragments.ComposeFragment;
+import com.example.bark.fragments.PostsFragment;
+import com.example.bark.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG= "MainActivity";
+    final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
 
     @Override
@@ -15,6 +25,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+                        fragment= new PostsFragment();
+                        Toast.makeText(MainActivity.this, "HOME!",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_compose:
+                        fragment= new ComposeFragment();
+                        Toast.makeText(MainActivity.this, "COMPOSE!",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profile:
+                    default:
+                        fragment= new ProfileFragment();
+                        Toast.makeText(MainActivity.this, "PROFILE!",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.action_home);
     }
+
 
 }
---
+++ app/src/main/java/com/example/bark/fragments/ComposeFragment.java
@@ -0,0 +1,50 @@
+package com.example.bark.fragments;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.example.bark.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ComposeFragment extends Fragment {
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE= 42;
+    private EditText etDescription;
+    private Button btnCaptureImage;
+    private ImageView ivPostImage;
+    private Button btnSubmit;
+
+
+    public ComposeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_compose, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnCaptureImage=view.findViewById(R.id.btnCapturePicture);
+        ivPostImage = view.findViewById(R.id.ivPostImage);
+        btnSubmit = view.findViewById(R.id.btnSubmit);
+        etDescription = view.findViewById(R.id.etDescription);
+    }
+}
