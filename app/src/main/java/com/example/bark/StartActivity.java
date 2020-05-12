49fbdd4ff8ed9d942e7e5039a2125ff411429191
larafonse <nlarafonseca@csumb.edu>
2020-05-11 18:06:34 -0700

    start slideshow added

--- app/src/main/java/com/example/bark/StartActivity.java
+++ app/src/main/java/com/example/bark/StartActivity.java
@@ -3,7 +3,10 @@ package com.example.bark;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ViewFlipper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +14,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class StartActivity extends AppCompatActivity {
+    Animation fade_in, fade_out;
+    ViewFlipper viewFlipper;
 
     Button login, register;
 
@@ -34,6 +39,20 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        viewFlipper = (ViewFlipper) this.findViewById(R.id.bckgrndViewFlipper1);
+
+        fade_in = AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_in);
+        fade_out = AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_out);
+
+        viewFlipper.setInAnimation(fade_in);
+        viewFlipper.setOutAnimation(fade_out);
+
+        //sets auto flipping
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(3000);
+        viewFlipper.startFlipping();
 
         login = findViewById(R.id.login);
         register = findViewById(R.id.register);
