417f17e9ba46a10f3ddb4150fe79f957adbfae94
*randrade8311* <randrade709@gmail.com>
2020-05-14 11:35:13 -0700

    Images now added to firebase

--- app/src/main/java/com/example/bark/AddPostActivity.java
+++ app/src/main/java/com/example/bark/AddPostActivity.java
@@ -9,6 +9,8 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -20,6 +22,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -35,6 +38,8 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class AddPostActivity extends AppCompatActivity {
@@ -46,24 +51,20 @@ public class AddPostActivity extends AppCompatActivity {
     private Button btnSubmit;
     private TextView cancelAdd;
     //private boolean pictureAdded;
-
+    private String timestamp;
     private FirebaseAuth mAuth;
-
-    ProgressDialog pd;
-    private Bitmap bitmap = null;
+    private ProgressDialog pd;
+    Uri downloadUri;
 
     //String username;
-    private String downLoadUrl;
     private String username;
-    private Uri image_uri = null;
-    private Task<Uri> image_bitmap_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
 
-        //pictureAdded = false;
+        timestamp = String.valueOf(System.currentTimeMillis());
         mAuth = FirebaseAuth.getInstance();
 
         btnCaptureImage = findViewById(R.id.btnCapturePicture);
@@ -100,90 +101,33 @@ public class AddPostActivity extends AppCompatActivity {
                     Toast.makeText(AddPostActivity.this, "Description cannot be empty!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                if(image_uri == null && bitmap==null){
+                if( ivPostImage==null){
                     Toast.makeText(AddPostActivity.this, "No image!", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                savePost(description, String.valueOf(image_uri), mAuth, db, bitmap);
+                savePost(description, mAuth, db);
             }
         });
 
     }
 
-    private void savePost(final String description, final String uri, FirebaseAuth mAuth, final FirebaseFirestore db, Bitmap bitmap) {
-        if(bitmap == null){
-            new ProgressDialog(AddPostActivity.this);
-            pd.setMessage("Publishing post...");
-            pd.show();
+    private void savePost(final String description, FirebaseAuth mAuth, final FirebaseFirestore db) {
+        new ProgressDialog(AddPostActivity.this);
+        pd.setMessage("Publishing post...");
+        pd.show();
 
-            final FirebaseUser firebaseUser = mAuth.getCurrentUser();
-            final String userID = firebaseUser.getUid();
+        final FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        final String userID = firebaseUser.getUid();
+        
+        username = getUserName(firebaseUser, db);
 
-            username = getUserName(firebaseUser, db);
-
-            final String timestamp = String.valueOf(System.currentTimeMillis());
-
-            final StorageReference reference = FirebaseStorage.getInstance().getReference()
-                    .child("postImages")
-                    .child("post_" + timestamp + ".jpeg");
-            reference.putFile(Uri.parse(uri))
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                            Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
-                            while(!uriTask.isSuccessful());
-
-                            downLoadUrl = uriTask.getResult().toString();
-
-                            if(uriTask.isSuccessful()){
-                                HashMap<String, Object> map = new HashMap<>();
-                                map.put("uId", userID);
-                                map.put("uName", firebaseUser.getDisplayName());
-                                map.put("postId", timestamp);
-                                map.put("description", description);
-                                map.put("image", downLoadUrl);
-                                db.collection("posts")
-                                        .add(map)
-                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                                            @Override
-                                            public void onSuccess(DocumentReference documentReference) {
-                                                Toast.makeText(AddPostActivity.this, "Post made", Toast.LENGTH_SHORT).show();
-                                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-                                                pd.dismiss();
-                                                Intent i = new Intent(AddPostActivity.this, MainActivity.class);
-                                                startActivity(i);
-                                            }
-                                        })
-                                        .addOnFailureListener(new OnFailureListener() {
-                                            @Override
-                                            public void onFailure(@NonNull Exception e) {
-                                                Toast.makeText(AddPostActivity.this, "Error posting", Toast.LENGTH_SHORT).show();
-                                                Log.w(TAG, "Error adding document", e);
-                                                pd.dismiss();
-                                            }
-                                        });
-                            }
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            pd.dismiss();
-                        }
-                    });
-        } else if (bitmap != null){
-            handleUpload(bitmap);
-
-            final FirebaseUser firebaseUser = mAuth.getCurrentUser();
-            final String userID = firebaseUser.getUid();
-            final String timestamp = String.valueOf(System.currentTimeMillis());
-
+        if(downloadUri!= null) {
             HashMap<String, Object> map = new HashMap<>();
             map.put("uId", userID);
             map.put("uName", firebaseUser.getDisplayName());
             map.put("postId", timestamp);
             map.put("description", description);
-            map.put("image", image_bitmap_uri);
+            map.put("image", downloadUri.toString());
             db.collection("posts")
                     .add(map)
                     .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
@@ -203,7 +147,12 @@ public class AddPostActivity extends AppCompatActivity {
                             Log.w(TAG, "Error adding document", e);
                             pd.dismiss();
                         }
-                    });}
+                    });
+            }
+        else{
+            pd.dismiss();
+            Toast.makeText(this, "Try again!", Toast.LENGTH_SHORT).show();
+        }
         }
 
 
@@ -237,16 +186,12 @@ public class AddPostActivity extends AppCompatActivity {
                         if(options[which].equals("Take Photo")) {
                             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                             if (intent.resolveActivity(getPackageManager()) != null) {
-                                image_uri = null;
                                 startActivityForResult(intent, 0);
                             }
                         }
                         else if (options[which].equals("Choose from Gallery")){
-                            bitmap = null;
-                            Intent intent = new Intent();
-                            intent.setType("image/*");
-                            intent.setAction(Intent.ACTION_GET_CONTENT);
-                            startActivityForResult(Intent.createChooser(intent, "Select image"), 1);
+                            Intent pickPhoto = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(pickPhoto , 1);
                         }
                         else if(options[which].equals("Cancel")){
                             dialog.dismiss();
@@ -262,53 +207,64 @@ public class AddPostActivity extends AppCompatActivity {
         if (requestCode == 0) {
             switch (resultCode) {
                 case RESULT_OK:
-                    bitmap = (Bitmap) data.getExtras().get("data");
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
                     //Set Image to your Profile
                     ivPostImage.setImageBitmap(bitmap);
+                    handleUpload();
             }
         }
         if (requestCode == 1) {
             switch (resultCode) {
                 case RESULT_OK:
-                    image_uri = data.getData();
-
-                    ivPostImage.setImageURI(image_uri);
+                    Bitmap bitmap = null;
+                    if (data != null) {
+                        try {
+                            bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+                        } catch (IOException e) {
+                            //Toast.makeText(AddPostActivity.this, "Error, " + e, Toast.LENGTH_SHORT).show();
+                            e.printStackTrace();
+                        }
+                    }
+                    ivPostImage.setImageBitmap(bitmap);
+                    handleUpload();
             }
         }
     }
-    private void handleUpload(Bitmap bitmap) {
+
+    private void handleUpload() {
+        final StorageReference reference = FirebaseStorage.getInstance().getReference()
+                .child("postImages")
+                .child(timestamp + ".jpeg");
+        // Get the data from an ImageView as bytes
+        ivPostImage.setDrawingCacheEnabled(true);
+        ivPostImage.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) ivPostImage.getDrawable()).getBitmap();
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
 
-        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        final StorageReference reference = FirebaseStorage.getInstance().getReference()
-                .child("profileImages")
-                .child(uid + ".jpeg");
-
-        reference.putBytes(baos.toByteArray())
-                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        image_bitmap_uri = getDownloadUrl(reference);
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.e(TAG, "onFailure: ",e.getCause() );
-                    }
-                });
-    }
-
-    private Task<Uri> getDownloadUrl(final StorageReference reference) {
-        reference.getDownloadUrl()
-                .addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        Log.d(TAG, "onSuccess: " + uri);
-
-                    }
-                });
-        return reference.getDownloadUrl();
+        UploadTask uploadTask = reference.putBytes(data);
+        Task<Uri> urlTask = uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+                // Continue with the task to get the download URL
+                return reference.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    downloadUri = task.getResult();
+                    //Toast.makeText(AddPostActivity.this, "Successful" + downloadUri, Toast.LENGTH_SHORT).show();
+                } else {
+                    // Handle failures
+                    // ...
+                    Toast.makeText(AddPostActivity.this, "Unsuccessful" + downloadUri, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
